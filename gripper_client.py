309a05a30721b137a3644dd967e4423495e98bbe
Jet Situ <situjet@gmail.com>
2022-12-01 17:14:51 -0800

    added gripper remote functionality

---
+++ gripper_client.py
@@ -0,0 +1,12 @@
+import socket
+
+def commandGripper(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 777)) #change this to the ip address
+    s.send(bytes(msg,"utf-8"))
+
+if __name__ == "__main__":
+    commandGripper("open")
+    commandGripper("close")
+    commandGripper("calibrate")
+    commandGripper("shutoff")
